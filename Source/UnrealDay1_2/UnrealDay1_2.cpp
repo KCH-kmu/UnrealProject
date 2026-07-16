f69ab00278461a3c7ae444da4c9e42fe043ed3af
unprotectleft <219069198+KCH-kmu@users.noreply.github.com>
2026-07-16 16:25:17 +0900

    AI가 밉다; 정상작동 확인 백업용 커밋

---
+++ Source/UnrealDay1_2/UnrealDay1_2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealDay1_2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealDay1_2, "UnrealDay1_2" );
