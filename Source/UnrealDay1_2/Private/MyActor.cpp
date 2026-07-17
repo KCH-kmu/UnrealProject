2e24e3390d6f742512441c6c746322bee961332c
unprotectleft <219069198+KCH-kmu@users.noreply.github.com>
2026-07-17 14:03:08 +0900

    중간저장 백업용

--- Source/UnrealDay1_2/Private/MyActor.cpp
+++ Source/UnrealDay1_2/Private/MyActor.cpp
@@ -2,6 +2,10 @@
 
 
 #include "MyActor.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/StaticMesh.h"
+#include "UObject/ConstructorHelpers.h"
 
 // Sets default values
 AMyActor::AMyActor()
@@ -9,6 +13,17 @@ AMyActor::AMyActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Root);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMesh(TEXT("/Engine/BasicShapes/Sphere.Sphere"));
+	if (SphereMesh.Succeeded())
+	{
+		Mesh->SetStaticMesh(SphereMesh.Object);
+	}
 }
 
 // Called when the game starts or when spawned
